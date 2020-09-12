370dc472df570277c7268986515eb87c0cc39d66
phansa <lekgaetonic@gmail.com>
2020-09-12 07:32:11 +0700

    m

--- lib/src/pages/home/main.dart
+++ lib/src/pages/home/main.dart
@@ -16,71 +16,71 @@ class HomePage extends StatelessWidget {
         preferredSize: Size.fromHeight(56.0), // here the desired height
         child: SharedAppBar(),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children:
-            buildListWidget(_homePageController.cmsHomePageModel.sections),
+      body: Obx(
+        () => Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:
+              buildListWidget(_homePageController.cmsHomePageModel.sections),
+        ),
       ),
     );
   }
 
   //https://stackoverflow.com/questions/56947046/flutter-for-loop-to-generate-list-of-widgets/56947190
   List<Widget> buildListWidget(data) {
-    return List.generate(data.length, (index) {
-      if (_homePageController.cmsHomePageModel.sections[index].type ==
-          "RotatingImagesComponent") {
-        return SizedBox(
-          height: 150.0,
-          // width: 300.0,
-          child: Carousel(
-            images: [
-              NetworkImage(
-                  'https://ktwdevapi.ktw.co.th/medias/sys_master/images/images/h8b/h1b/8845807026206/Happy-May-1900x650.jpg'),
-              NetworkImage(
-                  'https://ktwdevapi.ktw.co.th/medias/sys_master/images/images/h32/h4a/8845808205854/TOHO-1900x650s.jpg'),
-              NetworkImage(
-                  'https://ktwdevapi.ktw.co.th/medias/sys_master/images/images/h32/h4a/8845808205854/TOHO-1900x650s.jpg'),
-              NetworkImage(
-                  'https://shop.ktw.co.th/medias/sys_master/images/images/h65/hb9/9088749633566/5.-1900x650-3.jpg'),
-              // ExactAssetImage("assets/images/LaunchImage.jpg"),
-            ],
-          ),
-        );
-      } else if (_homePageController.cmsHomePageModel.sections[index].type ==
-          "SimpleBannerComponent") {
-        return ListTile(
-          leading: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].components.length ?? 0}'),
-          trailing: Icon(EvaIcons.activity),
-          subtitle: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].type}'),
-          title: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].section}'),
-        );
-      } else if (_homePageController.cmsHomePageModel.sections[index].type ==
-              "BannerComponent" ||
-          _homePageController.cmsHomePageModel.sections[index].type ==
-              "KTWBannerComponent") {
-        return ListTile(
-          leading: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].components.length ?? 0}'),
-          trailing: Icon(EvaIcons.activity),
-          subtitle: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].type}'),
-          title: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].section}'),
-        );
-      } else {
-        return ListTile(
-          leading: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].components.length ?? 0}'),
-          trailing: Icon(EvaIcons.activity),
-          subtitle: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].type}'),
-          title: Text(
-              '${_homePageController.cmsHomePageModel.sections[index].section}'),
-        );
-      }
-    });
+    print(data.length);
+    if (data != null) {
+      return List.generate(data.length, (index) {
+        if (data[index].type == "RotatingImagesComponent") {
+          return SizedBox(
+            height: 150.0,
+            // width: 300.0,
+            child: Carousel(
+              images: [
+                NetworkImage(
+                    'https://ktwdevapi.ktw.co.th/medias/sys_master/images/images/h8b/h1b/8845807026206/Happy-May-1900x650.jpg'),
+                NetworkImage(
+                    'https://ktwdevapi.ktw.co.th/medias/sys_master/images/images/h32/h4a/8845808205854/TOHO-1900x650s.jpg'),
+                NetworkImage(
+                    'https://ktwdevapi.ktw.co.th/medias/sys_master/images/images/h32/h4a/8845808205854/TOHO-1900x650s.jpg'),
+                NetworkImage(
+                    'https://shop.ktw.co.th/medias/sys_master/images/images/h65/hb9/9088749633566/5.-1900x650-3.jpg'),
+                // ExactAssetImage("assets/images/LaunchImage.jpg"),
+              ],
+              dotSize: 5.0,
+              indicatorBgPadding: 5.0,
+              dotBgColor: Colors.transparent,
+              dotColor: Colors.black38,
+              dotIncreasedColor: Colors.black54,
+            ),
+          );
+        } else if (data[index].type == "SimpleBannerComponent") {
+          return ListTile(
+            leading: Text('${data[index].components.length ?? 0}'),
+            trailing: Icon(EvaIcons.activity),
+            subtitle: Text('${data[index].type}'),
+            title: Text('${data[index].section}'),
+          );
+        } else if (data[index].type == "BannerComponent" ||
+            _homePageController.cmsHomePageModel.sections[index].type ==
+                "KTWBannerComponent") {
+          return ListTile(
+            leading: Text('${data[index].components.length ?? 0}'),
+            trailing: Icon(EvaIcons.activity),
+            subtitle: Text('${data[index].type}'),
+            title: Text('${data[index].section}'),
+          );
+        } else {
+          return ListTile(
+            leading: Text('${data[index].components.length ?? 0}'),
+            trailing: Icon(EvaIcons.activity),
+            subtitle: Text('${data[index].type}'),
+            title: Text('${data[index].section}'),
+          );
+        }
+      });
+    } else {
+      return List<Widget>();
+    }
   }
 }
